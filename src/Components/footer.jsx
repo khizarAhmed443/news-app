00dd9c688c11eeed7c0f50eea0eb5306fcb68acd
khizarAhmed443 <khizarahmed443@gmail.com>
2023-05-22 05:36:32 +0500

    Add footer

---
+++ src/Components/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p className="footer-text">Created by: Khizar Ahmed</p>
+        <p className="footer-text">Date: 22 May 2023</p>
+        <p className="footer-text">Contact: +92 306 8217407</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
