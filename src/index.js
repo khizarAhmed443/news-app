035d606ca27206f05eeb4a890711840c214eb974
khizarAhmed443 <khizarahmed443@gmail.com>
2023-05-22 05:55:26 +0500

    add home page which include 3 sections

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/footer";
+import NewsApp from "./Components/NewsApp";
+import { NewsProvider } from "./Components/NewsContext";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Navbar />
-    <Footer />
+   <NewsProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<NewsApp />} />
+        </Routes>
+        <Footer/>
+      </Router>
+    </NewsProvider>
   </React.StrictMode>
 );
